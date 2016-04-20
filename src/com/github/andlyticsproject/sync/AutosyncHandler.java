d7ac95e2bb0e9f94921ebae031e14019a1be7147
kwangsu <kwangsu@kwangsuui-MacBook-Air.local>
2016-04-20 22:06:27 +0900

    change default preferences

--- src/com/github/andlyticsproject/sync/AutosyncHandler.java
+++ src/com/github/andlyticsproject/sync/AutosyncHandler.java
@@ -1,17 +1,17 @@
 
 package com.github.andlyticsproject.sync;
 
-import java.util.List;
-
 import android.accounts.Account;
 import android.content.ContentResolver;
 import android.content.PeriodicSync;
 import android.os.Bundle;
 
+import java.util.List;
+
 
 public class AutosyncHandler {	
 
-	public static int DEFAULT_PERIOD = 60 * 3; // 3 hours (in minutes)
+	public static int DEFAULT_PERIOD = 0; //60 * 3; // 3 hours (in minutes)
 	public static final String ACCOUNT_AUTHORITY = "com.github.andlyticsproject";
 	public static final String ACCOUNT_TYPE_GOOGLE = "com.google";
 
